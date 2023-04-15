8ecc85cf84a8f251ddbe15839e8fab07a1a38ed0
SELIMCNR <btk02672@gmail.com>
2023-04-15 13:06:04 +0300

    Add project files.

---
+++ ExcelKayıtAktarma/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ExcelKayıtAktarma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Text_ogrno = new System.Windows.Forms.TextBox();
+            this.Text_adsoyad = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Text_KayıtTarihi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ogrno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Bölümü";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kayıt Tarihi";
+            // 
+            // Text_ogrno
+            // 
+            this.Text_ogrno.Location = new System.Drawing.Point(175, 56);
+            this.Text_ogrno.Name = "Text_ogrno";
+            this.Text_ogrno.Size = new System.Drawing.Size(196, 26);
+            this.Text_ogrno.TabIndex = 4;
+            // 
+            // Text_adsoyad
+            // 
+            this.Text_adsoyad.Location = new System.Drawing.Point(175, 102);
+            this.Text_adsoyad.Name = "Text_adsoyad";
+            this.Text_adsoyad.Size = new System.Drawing.Size(196, 26);
+            this.Text_adsoyad.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GRAFİK TASARIM ",
+            "BİLGİSAYAR MÜHENDİSLİĞİ",
+            "İŞLETME",
+            "ELEKTRİK VE ELEKTRONİK",
+            "OTOMASYON"});
+            this.comboBox1.Location = new System.Drawing.Point(175, 146);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(196, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Text_KayıtTarihi
+            // 
+            this.Text_KayıtTarihi.Location = new System.Drawing.Point(175, 187);
+            this.Text_KayıtTarihi.Name = "Text_KayıtTarihi";
+            this.Text_KayıtTarihi.Size = new System.Drawing.Size(196, 26);
+            this.Text_KayıtTarihi.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 53);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 292);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(632, 287);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 53);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Excele aktar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 53);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(336, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 53);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Güncelle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 581);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Text_KayıtTarihi);
+            this.Controls.Add(this.Text_adsoyad);
+            this.Controls.Add(this.Text_ogrno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Text_ogrno;
+        private System.Windows.Forms.TextBox Text_adsoyad;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Text_KayıtTarihi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ ExcelKayıtAktarma/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelKayıtAktarma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Sql Bağlantı
+        public SqlConnection baglantı = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Ogrenci.mdf;Integrated Security=True");
+        //Sql komutu 
+        public SqlCommand komut = new SqlCommand();
+        //Data adapter veri adaptörü 
+        public SqlDataAdapter adaptör = new SqlDataAdapter();
+        //Data set veri seti
+        public DataSet dt = new DataSet();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gridDoldur();
+        }
+        void gridDoldur()
+        {
+            //Adatörle bağlantı yapıldı
+            adaptör = new SqlDataAdapter("Select * from Ogrenci ", baglantı);
+            //Datasete veri aktarıldı
+            dt= new DataSet();
+            //Bağlantı açıldı
+            baglantı.Open();
+            //Adaptör ile data seti doldur ogrenci tablosu ile
+            adaptör.Fill(dt, "Ogrenci");
+            //DataGridviewin kaynadğına data sete verileri aktar.
+            dataGridView1.DataSource = dt.Tables["Ogrenci"];
+            baglantı.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  //Kaydetme
+            baglantı.Open();
+            komut.Connection = baglantı;
+            komut.CommandText = "insert into ogrenci(NUMARA,ADSOYAD,BOLUM,KAYITTARIHI) values('"+Text_ogrno.Text+ "','"+Text_adsoyad.Text+ "','"+comboBox1.Text+ "','"+Text_KayıtTarihi.Text+"')";
+            komut.ExecuteNonQuery();
+            baglantı.Close() ;
+            MessageBox.Show("Kayıt eklendi.");
+            gridDoldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+                //excele kaydet
+                //Excel kütüphanesini aktife getir
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = true; // exceli aç 
+                object Missing = Type.Missing; //Uyarıları çalıştır
+                Workbook workbook = excel.Workbooks.Add(Missing);
+                Worksheet sheet = (Worksheet)workbook.Sheets[1];
+                int satırRow = 1;
+                int sutunCol = 1;
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    Range myRange = (Range)sheet.Cells[sutunCol, satırRow + j];
+                    myRange.Value2 = dataGridView1.Columns[j].HeaderText;
+                }
+                sutunCol++;
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        Range myrange = (Range)sheet.Cells[sutunCol + i, satırRow + j];
+                        myrange.Value2 = dataGridView1[j, i].Value == null ? " " : dataGridView1[j, i].Value;
+                        myrange.Select();
+                    }
+
+                }
+         
+        
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //güncelle
+            baglantı.Open();
+            komut.Connection = baglantı;
+            komut.CommandText = "update Ogrenci set ADSOYAD='"+Text_adsoyad.Text+"',BOLUM='"+comboBox1.Text+"',KAYITTARIHI='"+Text_KayıtTarihi.Text+"' where NUMARA ='" + Text_ogrno.Text + "'";
+            komut.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("Kayıt güncellendi.");
+            gridDoldur();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Sil 
+            baglantı.Open();
+            komut.Connection = baglantı;
+            komut.CommandText = "Delete from ogrenci where NUMARA = '"+Text_ogrno.Text+"'";
+            komut.ExecuteNonQuery();
+            baglantı.Close();
+            MessageBox.Show("Kayıt silindi.");
+            gridDoldur();
+        }
+    }
+}
